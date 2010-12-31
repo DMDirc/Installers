24a8d2ff90e273c5a83f38e19a566d97f8aebc9c
Shane Mc Cormack <Dataforce@Dataforce.org.uk>
2010-12-31 23:39:11 +0000

    Update Copyright
    Change-Id: I7fc265e769f763596a740d49a9fd516a1329c344
    Reviewed-on: http://gerrit.dmdirc.com/1722
    Reviewed-by: Chris Smith <chris@dmdirc.com>
    Automatic-Compile: DMDirc Local Commits <dmdirc@googlemail.com>

--- tests/testDebianVersion.sh
+++ tests/testDebianVersion.sh
@@ -9,7 +9,7 @@
 # This appears to work ok.
 #
 # DMDirc - Open Source IRC Client
-# Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+# Copyright (c) 2006-2011 Chris Smith, Shane Mc Cormack, Gregory Holmes
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
